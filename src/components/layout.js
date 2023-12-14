964814a0e93e0d4ad26ce07303580f1f38b9eca5
Keith Solomon <keith@vincentdesign.ca>
2023-12-14 12:41:14 -0600

    Initial commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -35,7 +35,7 @@ const Layout = ({ isHomePage, children }) => {
       <main>{children}</main>
 
       <footer>
-        © {new Date().getFullYear()}, Built with
+        © {new Date().getFullYear()}, Keith Solomon, all rights reserved.  Built with
         {` `}
         <a href="https://www.gatsbyjs.com">Gatsby</a>
         {` `}
